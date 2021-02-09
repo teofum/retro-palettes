8dd6745a87da1255245f825e52376feb6cc3edd5
Teo Fumagalli <teo.fum@gmail.com>
2021-02-08 21:36:18 -0300

    CIELab mode now caches converted colors, should no longer be unbearably slow on large palettes and gets faster the more you use it

--- src/colorCompare/ColorCompareFn.ts
+++ src/colorCompare/ColorCompareFn.ts
@@ -8,9 +8,26 @@ export function colDistRGB(color1: number[], color2: number[]): number {
   return vec3distance(color1, color2);
 }
 
+const labCache: { [key: number]: number[] } = {};
+
 // Computes color distance in CIE-L*ab space. Slower, but more visually accurate.
 export function colDistLab(color1: number[], color2: number[]): number {
-  return vec3distance(srgb2lab(color1), srgb2lab(color2));
+  const c1index = color1[0] + (color1[1] << 8) + (color1[2] << 16);
+  const c2index = color2[0] + (color2[1] << 8) + (color2[2] << 16);
+
+  let lab1 = labCache[c1index];
+  if (!lab1) {
+    lab1 = srgb2lab(color1);
+    labCache[c1index] = lab1;
+  }
+  
+  let lab2 = labCache[c2index];
+  if (!lab2) {
+    lab2 = srgb2lab(color2);
+    labCache[c2index] = lab2;
+  }
+
+  return vec3distance(lab1, lab2);
 }
 
 export default ColorDistanceFn;
