f9fe48996b54043f662a288d739b063312c4c4c4
Teo Fumagalli <teo.fum@gmail.com>
2021-02-15 18:57:32 -0300

    Added save, view original buttons to result window

--- src/ui/RenderWindow.ts
+++ src/ui/RenderWindow.ts
@@ -8,6 +8,8 @@ class RenderWindow extends UIWindow {
   private flash: HTMLElement;
   private renderer: Renderer;
 
+  private originalButton: HTMLButtonElement;
+
   public get inputCanvas(): HTMLCanvasElement { return this.inputCanvasRef; }
   public get outputCanvas(): HTMLCanvasElement { return this.outputCanvasRef; }
 
@@ -33,6 +35,32 @@ class RenderWindow extends UIWindow {
 
     this.content.appendChild(canvasContainer);
 
+    // Append a menu bar
+    const saveIcon = document.createElement('img');
+    saveIcon.src = './public/img/save.png';
+
+    const saveButton = document.createElement('button');
+    saveButton.className = 'win-bevel hover light icon-button';
+    saveButton.title = 'Save';
+    saveButton.addEventListener('click', this.save);
+    saveButton.appendChild(saveIcon);
+
+    const originalIcon = document.createElement('img');
+    originalIcon.src = './public/img/original.png';
+
+    this.originalButton = document.createElement('button');
+    this.originalButton.className = 'win-bevel hover light icon-button';
+    this.originalButton.title = 'View original image';
+    this.originalButton.addEventListener('click', this.toggleViewOriginal);
+    this.originalButton.appendChild(originalIcon);
+
+    const menuBar = document.createElement('div');
+    menuBar.className = 'menubar';
+    menuBar.appendChild(saveButton);
+    menuBar.appendChild(this.originalButton);
+
+    this.frame.appendChild(menuBar);
+
     // Create the renderer
     this.renderer = new Renderer(this.inputCanvas, this.outputCanvas);
 
@@ -45,6 +73,28 @@ class RenderWindow extends UIWindow {
     return this.renderer.render(image, options).then(() => {
       this.flash.classList.add('flash-anim');
     });
+  }
+
+  public save = (): void => {
+    const dataUrl = this.outputCanvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+    const link = document.createElement('a');
+    link.setAttribute('download', 'result.png');
+    link.setAttribute('href', dataUrl);
+    link.click();
+  }
+
+  public toggleViewOriginal = (): void => {
+    if (this.outputCanvas.classList.contains('clip')) {
+      this.outputCanvas.classList.remove('clip');
+      this.originalButton.classList.remove('inset');
+      this.originalButton.classList.remove('checkered');
+      this.originalButton.classList.add('hover');
+    } else {
+      this.outputCanvas.classList.add('clip');
+      this.originalButton.classList.add('inset');
+      this.originalButton.classList.add('checkered');
+      this.originalButton.classList.remove('hover');
+    }
   }
 }
 
