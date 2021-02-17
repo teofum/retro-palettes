02dcbec0e1c1169bf9086af6a5fdc37dc8508f77
Teo Fumagalli <teo.fum@gmail.com>
2021-02-17 02:33:26 -0300

    Checkbox fix

--- src/ui/makeFakeCheckbox.ts
+++ src/ui/makeFakeCheckbox.ts
@@ -1,16 +1,28 @@
 export function makeFakeCheckbox(input: HTMLInputElement): void {
   const fakeCheckbox = document.createElement('div');
   fakeCheckbox.className = 'win-bevel checkbox fake-checkbox';
-  fakeCheckbox.style.backgroundImage = input.checked ?
-    'url(./public/img/check.png)' :
-    '';
+  input.checked ?
+      fakeCheckbox.classList.add('checked') :
+      fakeCheckbox.classList.remove('checked');
 
-  fakeCheckbox.addEventListener('click', () => {
-    input.click();
-    fakeCheckbox.style.backgroundImage = input.checked ?
-      'url(./public/img/check.png)' :
-      '';
+  fakeCheckbox.addEventListener('click', () => input.click());
+  input.addEventListener('change', () => {
+    input.checked ?
+      fakeCheckbox.classList.add('checked') :
+      fakeCheckbox.classList.remove('checked');
   });
+  
+  const config = { attributes: true, childList: false, subtree: false };
+  const disabledObserver = new MutationObserver((mutations => {
+    mutations.forEach(mutation => {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'disabled')
+        input.disabled ?
+          fakeCheckbox.setAttribute('disabled', 'true') :
+          fakeCheckbox.removeAttribute('disabled');
+    });
+  }));
+
+  disabledObserver.observe(input, config);
 
   input.before(fakeCheckbox);
   input.hidden = true;
